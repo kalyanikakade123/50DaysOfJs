3858ff6d2998644df1611d611f9a994ebd7324a3
Kalyani Kakade <86308103+kalyanikakade123@users.noreply.github.com>
2022-02-04 21:34:53 -0800

    Update getDivisorsCnt.js

--- Day 44/getDivisorsCnt.js
+++ Day 44/getDivisorsCnt.js
@@ -1,11 +1,11 @@
 function getDivisorsCnt(num) {
     let count = 0;
-    for (let i = 1; i <= 100000; i++){
+    for (let i = 1; i <= num; i++){
         if(num%i == 0){
             count++;
         }
     } 
-    return count++;
+    return count;
 }
-console.log(getDivisorsCnt(900))
+
   
